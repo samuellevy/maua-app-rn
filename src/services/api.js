3500d10c04278edb8f86a974787a4d7f26ad99a7
Samuel Levy <samuellevy@3AWW13.local>
2018-07-18 15:01:13 -0300

    build001 playstore google android

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import { AsyncStorage } from 'react-native';
 import { create } from  'apisauce';
 
 const api = create({
-    baseURL: 'http://192.168.2.71/rest/api',
-    // baseURL: 'http://dev2.3aww.com.br/lafargemaua/api',
+    // baseURL: 'http://192.168.2.71/rest/api',
+    baseURL: 'http://dev2.3aww.com.br/lafargemaua/api',
     // baseURL: 'http://192.168.0.27/lafarge-backend/api',
 });
 
