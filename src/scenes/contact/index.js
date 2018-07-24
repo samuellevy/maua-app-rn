2c44b2a8a4a34e278ad444830c860b6f281d389d
Samuel Levy <samuellevy@3AWW13.local>
2018-07-24 02:54:06 -0300

    sending contact

--- src/scenes/contact/index.js
+++ src/scenes/contact/index.js
@@ -5,6 +5,7 @@ import { colors } from '../../styles';
 import { View } from 'react-native';
 import Title from '../../components/title/primary';
 import Button from '../../components/button';
+import rest from '../../services/rest';
 
 import styles from './styles';
 
@@ -12,6 +13,18 @@ export default class Contact extends Component {
   constructor(props) {
     super(props);
     this.state = { text: '' };
+  }
+
+  sendData(){
+    let contact = JSON.stringify({
+        text: this.state.text
+      });
+
+    rest.post('/public/contact', contact).then((rest)=>{
+        console.log(rest);
+    });
+
+    alert('Enviado com sucesso!');
   }
 
   render() {
@@ -23,11 +36,11 @@ export default class Contact extends Component {
           </Text>
           <Text style={styles.txtDescription}>Possui algum comentário, dúvida ou sugestão? Entre em contato com a gente!</Text>
           <View style={styles.inputBox}>
-            <TextInput underlineColorAndroid='rgba(0,0,0,0)' style={styles.input} placeholder='Escreva sua mensagem' placeholderTextColor={colors.gray} multiline={true} onChangeText={(text) => this.setState({ text })} returnKeyType='send'/>
+            <TextInput underlineColorAndroid='rgba(0,0,0,0)' style={styles.input} placeholder='Escreva sua mensagem' placeholderTextColor={colors.gray} multiline={true} onChangeText={(text) => this.setState({ text })} blurOnSubmit={true} autoCorrect={false} returnKeyType={'done'}/>
           </View>
         </View>
         <View style={styles.boxSend}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={()=>{this.sendData()}}>
             <Button title={'Enviar mensagem'} size={60} color={colors.regular} style={[styles.boxSend, this.state.text!=null?{backgroundColor:colors.yellow}:{backgroundColor:colors.gray}]}/>
           </TouchableOpacity>
         </View>
