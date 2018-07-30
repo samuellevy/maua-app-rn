6cb7f8f267c3efc151991860c47c6526ff164e73
Samuel Levy <samuellevy@3AWW13.local>
2018-07-30 14:49:05 -0300

    build ios with ipad configurations

--- src/services/rest.js
+++ src/services/rest.js
@@ -1,8 +1,8 @@
 import { AsyncStorage } from 'react-native';
 
 var rest = {
-    url: 'http://192.168.2.71/rest/api',
-    // url: 'http://dev2.3aww.com.br/lafargemaua/api',
+    // url: 'http://192.168.2.71/rest/api',
+    url: 'http://dev2.3aww.com.br/lafargemaua/api',
     async get(action){
         const token = await AsyncStorage.getItem("@CodeApi:token");
         var url = this.url + action;
