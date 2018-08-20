11411b2cf901e9a9a23288014b99cd293fcbd17a
ronalsilva <ronaldjunger@gmail.com>
2018-08-20 16:51:50 -0300

    buttom back android

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import { StackNavigator } from 'react-navigation';
 import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { View, BackAndroid } from 'react-native';
 import MainNavigator from './MainNavigator';
 
 export default class App extends Component {
-  render() {
-    return (
-      <MainNavigator />
-    );
-  }
+    componentDidMount() {
+        BackAndroid.addEventListener('hardwareBackPress', this.handleBackButton);
+        // Alert.alert("No Users Found", "Oops, Looks like you are not signed in");
+    }
+      
+    componentWillUnmount() {
+        BackAndroid.removeEventListener('hardwareBackPress', this.handleBackButton);
+    }
+
+    handleBackButton() {
+        return true;
+	}
+	
+  	render() {
+	    return (
+      		<MainNavigator />
+    	);
+  	}
 }
