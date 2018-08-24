adae75839da550f923c5f4d51c463fe7e3179d1f
ronalsilva <ronaldjunger@gmail.com>
2018-08-24 12:04:12 -0300

    tirando o backAndroid e colocando o BackHandler

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import { StackNavigator } from 'react-navigation';
 import React, { Component } from 'react';
 
-import { View, BackAndroid } from 'react-native';
+import { View, BackAndroid, BackHandler } from 'react-native';
 import MainNavigator from './MainNavigator';
 
 export default class App extends Component {
     componentDidMount() {
-        BackAndroid.addEventListener('hardwareBackPress', this.handleBackButton);
-        // Alert.alert("No Users Found", "Oops, Looks like you are not signed in");
-    }
-      
-    componentWillUnmount() {
-        BackAndroid.removeEventListener('hardwareBackPress', this.handleBackButton);
-    }
+        BackHandler.addEventListener('backPress', () => {
+            const { dispatch, nav } = this.props;
 
-    handleBackButton() {
-        return true;
-	}
+            
+            return true;
+        });
+    }
+    
+    componentWillUnmount() {
+        BackHandler.removeEventListener('backPress')
+    }
 	
   	render() {
 	    return (
